faff79dc2fe8fd78936f54eb0f0f1fce61ef7cd2
Hamad Alkhal <46822125+hamadsuniverse@users.noreply.github.com>
2022-04-11 00:32:19 +0300

    Add STATIC_ROOT

--- trafficpollution/settings.py
+++ trafficpollution/settings.py
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "dashboard/static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
