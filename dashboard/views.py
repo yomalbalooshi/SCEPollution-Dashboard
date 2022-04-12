3a0c44d812231ef4cc30ced7fea2abf5a4338bbe
Zakeya <20185035@stu.uob.edu.bh>
2022-04-12 18:31:14 +0300

    Revert "Update views.py"
    This reverts commit 93daa643748d291a4ac50fc8d9c2230c5a481ca6.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,5 @@
 from django.shortcuts import render
-import boto3
-import time
-import datetime
-import random
-from django.contrib.sessions.models import Session
 
-DB_NAME = "dummyDB"
-
-TBL_NAME = "sensorReadings"
 
 def index(request):
     return render(request, "dashboard/index.html")
-client = boto3.client('timestream-query')
-def qu():
-    response = client.query(QueryString = 'SELECT * FROM "dummyDB"."sensorReadings" WHERE time between ago(15m) and now() ORDER BY time DESC LIMIT 10 ')
-    return render(response, "dashboard/index.html")
